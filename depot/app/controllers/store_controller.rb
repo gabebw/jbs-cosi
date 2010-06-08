e39bca8cb3256eb90f03486deee41a91dd39e0ca
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-08 00:57:33 -0400

    Add error handling, CartItem model, and show flash notices in store layout

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -5,13 +5,30 @@ class StoreController < ApplicationController
   end
   
   def add_to_cart
-    product = Product.find(params[:id])
-    @cart = find_cart
-    @cart.add_product(product)
+    begin
+      product = Product.find(params[:id])
+      @cart = find_cart
+      @cart.add_product(product)
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      flash[:notice] = "Invalid product"
+      redirect_to :action => 'index'
+    end
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    flash[:notice] = "Your cart is currently empty"
+    redirect_to :action => 'index'
   end
 
   private # private methods are not available as actions
   def find_cart
     session[:cart] ||= Cart.new
+  end
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => 'index'
   end
 end
