2a8c7548a0fdf8313987ac2dcebeb992e65a3cd2
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-08 22:54:31 -0400

    Add hidden_div_if helper method and use it in store layout

--- depot/app/helpers/store_helper.rb
+++ depot/app/helpers/store_helper.rb
@@ -1,2 +1,8 @@
 module StoreHelper
+  # Generate a <div> which is hidden if condition evaluates to true
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes['style'] = "display: none"
+    end
+    content_tag("div", attributes, &block)
 end
