8fa29d87afa4287c5b798afc5fcb52bed81950f9
Gabe Berke-Williams <gbw@brandeis.edu>
2010-06-21 19:16:22 -0400

    Add dictionary.rb for testing

---
+++ hw12/dictionary/dictionary.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+# coding: utf-8 # required for using accented chars
+
+require 'test/unit'
+
+# Design a class to represent a dictionary
+class Dictionary
+  
+  # We know that the dictionary starts out empty
+  def initialize
+    @dictionary = {}
+  end
+
+  # Don't know yet how I will represent info
+  def empty?
+    @dictionary.empty?
+  end
+ 
+  # Translations work both ways
+  def add_translation(from, to)
+    @dictionary[to] = from
+    @dictionary[from] = to
+  end
+  
+  def translate(word)
+    @dictionary[word]
+  end
+end
+
+# Test the class as we develop it
+class DictionaryTest < Test::Unit::TestCase
+  
+  def setup
+    @dict = Dictionary.new
+  end
+
+# Check that a new dictionary is empty
+  def test_empty_dict
+    assert @dict .empty?
+  end
+
+  # Check that once you add at least one translation it is not empty  
+  def test_adding_xlate
+    @dict.add_translation("book", "boek")
+    assert !@dict.empty?
+  end
+
+  # Check that I can fetch the translation I adde@dict.
+  def test_add_fetch_xlate
+    @dict.add_translation("book", "boek")
+    book = @dict.translate("boek")
+    assert_equal "book", book, "expected translation to be book"
+  end
+  
+  # Let's check two translations
+  def test_add_two_xlates
+    @dict.add_translation("book", "boek")
+    @dict.add_translation("house", "huis")
+    assert !@dict.empty?
+    assert_equal "book", @dict.translate("boek")
+    assert_equal "house", @dict.translate("huis")
+  end
+  
+  def test_accented
+    @dict.add_translation("book", "böek".encode("UTF-8"))
+    assert !@dict.empty?
+    assert_equal "book", @dict.translate("böek")
+  end
+
+  # An added translation should work both ways
+  def test_bidirectional_xlate
+    assert @dict.empty?
+    @dict.add_translation("book", "boek")
+    assert !@dict.empty?
+    assert_equal "book", @dict.translate("boek")
+    assert_equal "boek", @dict.translate("book")
+  end
+end
