fb3074873a602fb9cb3285eb8be3444d9299e770
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-08 19:25:10 -0400

    R.java

--- hw19/MyMap/gen/org/example/mymap/R.java
+++ hw19/MyMap/gen/org/example/mymap/R.java
@@ -14,15 +14,24 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int frame=0x7f050000;
-        public static final int go_button=0x7f050001;
-        public static final int map=0x7f050003;
-        public static final int position=0x7f050002;
+        public static final int frame=0x7f060000;
+        public static final int go_button=0x7f060001;
+        public static final int map=0x7f060003;
+        public static final int position=0x7f060002;
+        public static final int satellite_button=0x7f060006;
+        public static final int street_view_button=0x7f060005;
+        public static final int traffic_button=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int menu=0x7f050000;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int satellite_label=0x7f040001;
+        public static final int street_view_label=0x7f040002;
+        public static final int traffic_label=0x7f040003;
     }
 }
