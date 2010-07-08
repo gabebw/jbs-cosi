968d17521c35851f0f6dd4a7984bb410a02b7731
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-08 19:36:52 -0400

    Add preference activity

--- hw19/MyMap/gen/org/example/mymap/R.java
+++ hw19/MyMap/gen/org/example/mymap/R.java
@@ -14,24 +14,35 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int frame=0x7f060000;
-        public static final int go_button=0x7f060001;
-        public static final int map=0x7f060003;
-        public static final int position=0x7f060002;
-        public static final int satellite_button=0x7f060006;
-        public static final int street_view_button=0x7f060005;
-        public static final int traffic_button=0x7f060004;
+        public static final int frame=0x7f070000;
+        public static final int go_button=0x7f070001;
+        public static final int map=0x7f070003;
+        public static final int position=0x7f070002;
+        public static final int satellite_button=0x7f070006;
+        public static final int street_view_button=0x7f070005;
+        public static final int traffic_button=0x7f070004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int satellite_label=0x7f040001;
-        public static final int street_view_label=0x7f040002;
-        public static final int traffic_label=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int satellite_label=0x7f050001;
+        public static final int settings_label=0x7f050004;
+        public static final int street_view_label=0x7f050002;
+        public static final int summary_satellite_preference=0x7f050009;
+        public static final int summary_street_view_preference=0x7f050007;
+        public static final int summary_traffic_preference=0x7f05000b;
+        public static final int title_satellite_preference=0x7f050008;
+        public static final int title_street_view_preference=0x7f050006;
+        public static final int title_traffic_preference=0x7f05000a;
+        public static final int traffic_label=0x7f050003;
+        public static final int view_preferences=0x7f050005;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
